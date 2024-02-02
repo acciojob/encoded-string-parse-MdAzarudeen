975c356225eb03302c8877777e20c059899d1a24
MdAzarudeen <142100127+MdAzarudeen@users.noreply.github.com>
2024-02-02 20:53:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// Write your script here
+
 const parseCode = (str) => {
   // your code here
+
+  const [firstName, lastName, id] = str.split("0").filter(Boolean);
+  return { firstName, lastName, id };
 };
 
 // Do not change the code below
